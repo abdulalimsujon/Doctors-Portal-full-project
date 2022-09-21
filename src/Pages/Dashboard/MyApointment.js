dd8e4a6c30ca3466e9015847cf20369d2c89d614
abdulalimsujon <alimsujon12@gmail.com>
2022-09-21 13:54:01 +0600

    dashboard count

--- src/Pages/Dashboard/MyApointment.js
+++ src/Pages/Dashboard/MyApointment.js
@@ -1,9 +1,31 @@
+
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
 
 const MyApointment = () => {
+    const [appointments, setAppointments] = useState([]);
+    const [user] = useAuthState(auth);
+
+
+    useEffect(() => {
+
+        if (user) {
+            fetch(`http://localhost:5000/booking?patient=${user.email}`)
+                .then(res => res.json())
+                .then(data => setAppointments(data));
+        }
+
+    }, [user])
+
+
+
     return (
         <div>
-            <h2>My appointment</h2>
+            <h2>My appointment {appointments.length}</h2>
         </div>
     );
 };
